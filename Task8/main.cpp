fdf9890979485580764248de4545a2a6378b561b
thomasheath3 <thomas.heath-3@students.plymouth.ac.uk>
2021-11-23 15:55:44 +0000

    23/11/21 2

--- Task8/main.cpp
+++ Task8/main.cpp
@@ -30,7 +30,12 @@ void displayArray(int rows, int cols, double *Array)
 //-41 -45 -49  -53
 // 9  108 119  130
 //-25 -27 -29  -31
-double y[4][4];
+double Y[4][4] = {  //creating a blank 4x4 matrix for Y
+    {0.0, 0.0, 0.0, 0.0},
+    {0.0, 0.0, 0.0, 0.0},
+    {0.0, 0.0, 0.0, 0.0},
+    {0.0, 0.0, 0.0, 0.0},
+};
 // ***** For (3) WRITE YOUR FUNCTION HERE *****
 
 // *****  END YOUR FUNCTION HERE  *****
@@ -41,14 +46,26 @@ int main()
     displayArray(4,3,(double*)M);           //Display in terminal
 
     // ***** MODIFY THE CODE BELOW HERE *****
-
     // 1. Create another Array Y with the correct dimensions to hold the result of M*N (see comments above)
 
     // 2. Write a nested loop to perform a matrix multiplication M*N and store the result in Y
-    for(int i = 0; i <= 3; i++){
-        for(int x = 0; x <= 2; x++){
+    for(int i = 0; i <= 3; i++){// loops 4 time, same number of rows as M
+        for(int s = 0; s<=3; s++){ //loops 4 times to create each row for Y
+            int value = 0;
+            for(int x = 0; x <= 2; x++){ // loops 3 times as 3 additions have to be made
+                value += M[i][x] * N[x][s]; // adding specific values from the two matrix together, the sum of 3 additions
+            }
+            
+            Y[i][s] = value; // assignes the value of the additions to the correct row and column of Y 
             
         }
+
+    }
+    for(int z = 0; z<=3; z++){ // loops 4 times, number of rows for Y
+        for(int j = 0; j<=3; j++){ // loops number of rows for Y
+            printf("%f ", Y[z][j]); // this prints each row of Y and then a new line to present all values in matrix form
+        }
+        printf("\n");
     }
     // 3. Write a function to multiply two matrices together (see comments above). Include some text code to demonstrate it working
 
